a15871d8ba3a9b919914a91a05691f36ba4da56f
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2019-06-02 16:09:08 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import song from './modals/TamBietNhe.mp3';
- 
+import data from './modals/data.json';
+
 // Component
 import Header from './components/Header/Header';
 import Image from './components/Image/Image';
-
-const modalImageStyle = {
-  position: "fixed",
-  width: "100%",
-  height: "100vh",
-  background: "rgb(0, 0, 0, 0.75)",
-  top: "0px",
-  bottom: "0px",
-  zIndex: 2,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-}
-
-const imageStyle = {
-  width: "150%"
-}
-
 
 class App extends Component {
   constructor() {
@@ -34,17 +17,12 @@ class App extends Component {
     }
     this.audio = new Audio(song);
     // this.audio.autoplay = true;
-  }
-
-  showImage = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+    this.audio.loop = true;
   }
 
   togglePlay = () => {
-    this.setState({ 
-      isMute: !this.state.isMute 
+    this.setState({
+      isMute: !this.state.isMute
     }, () => {
       if (this.state.isMute) {
         this.audio.pause();
@@ -54,49 +32,28 @@ class App extends Component {
     });
   }
 
-  onClose = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
   render() {
-    let modalImage = (
-      <div style={modalImageStyle}>
-        <Image style={imageStyle} />
-        <button onClick={this.onClose} className="close-button">Close</button>
-      </div>
+
+    let imageRender = (
+      data.image.map((image, index) => {
+        return (
+          <div key={index}>
+            <Image source={image.source} />
+          </div>
+        )
+      })
     )
-
-    if (!this.state.isOpen) {
-      modalImage = null;
-    }
-
-    console.log(modalImage);
 
     return (
       <div className="App">
         <Header />
 
         <div className="image-area">
-          <Image showImage={this.showImage} />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
+          {imageRender}
         </div>
-        
+
         <a className="back-to-top" href="#"><i className="fas fa-chevron-up"></i></a>
         <button onClick={this.togglePlay} className="button-stop">{this.state.isMute ? <i className="fas fa-play"></i> : <i className="fas fa-pause"></i>}</button>
-
-        {modalImage}
 
         <footer>
           <p>Món quà kỷ niệm anh em</p>
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -4,11 +4,44 @@ import './Image.css';
 // Component
 
 class Image extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  showImage = () => {
+    // alert("Chức năng sắp ra mắt");
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  onClose = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
-    return (
-      <div className="Image" onClick={this.props.showImage}>
-        <img src="https://picsum.photos/200/300?random=1" alt="" style={this.props.style} />
+    let modalImage = (
+      <div className="modal-image">
+        <img src={this.props.source} alt="" className="image-detail" />
+        <button onClick={this.onClose} className="close-button">x</button>
       </div>
+    )
+
+    if (!this.state.isOpen) {
+      modalImage = null;
+    }
+
+    return (
+      <div className="Image" onClick={this.showImage}>
+        <img src={this.props.source} alt="" />
+
+        {modalImage}
+      </div >
     );
   }
 }
